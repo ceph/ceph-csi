f273874f266290a985def430abe056a8e9d3f70e
malc0lm <malclee@outlook.com>
2018-06-13 15:14:15 +0800

    rbd: advertises PluginCapability_Service_CONTROLLER_SERVICE

--- pkg/rbd/identityserver.go
+++ pkg/rbd/identityserver.go
@@ -17,9 +17,26 @@ limitations under the License.
 package rbd
 
 import (
+	"context"
+
+	"github.com/container-storage-interface/spec/lib/go/csi/v0"
 	"github.com/kubernetes-csi/drivers/pkg/csi-common"
 )
 
 type identityServer struct {
 	*csicommon.DefaultIdentityServer
+}
+
+func (is *identityServer) GetPluginCapabilities(ctx context.Context, req *csi.GetPluginCapabilitiesRequest) (*csi.GetPluginCapabilitiesResponse, error) {
+	return &csi.GetPluginCapabilitiesResponse{
+		Capabilities: []*csi.PluginCapability{
+			{
+				Type: &csi.PluginCapability_Service_{
+					Service: &csi.PluginCapability_Service{
+						Type: csi.PluginCapability_Service_CONTROLLER_SERVICE,
+					},
+				},
+			},
+		},
+	}, nil
 }
