1585c0dd3670343154e7eb1c610968304b2a4c4b
Kevin Fox <Kevin.Fox@pnnl.gov>
2019-02-06 09:15:19 -0800

    Update wrong repo name
    The origional name was wrong. Change it to the correct name.
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- deploy.sh
+++ deploy.sh
@@ -13,27 +13,27 @@ if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]
     chmod 700 get_helm.sh
     ./get_helm.sh
 
-    git clone https://github.com/ceph/csi-ceph
+    git clone https://github.com/ceph/csi-charts
     git clone https://github.com/ceph/ceph-csi
 
-    mkdir -p csi-ceph/docs
+    mkdir -p csi-charts/docs
     pushd ceph-csi > /dev/null
     git checkout -b csi-v1.0
 
     CHANGED=0
     VERSION=$(cat deploy/rbd/helm/Chart.yaml | awk '{if(/^version:/){print $2}}')
 
-    if [ ! -f "../csi-ceph/docs/ceph-csi-rbd-$VERSION.tgz" ]; then
+    if [ ! -f "../csi-charts/docs/ceph-csi-rbd-$VERSION.tgz" ]; then
         CHANGED=1
         ln -s deploy/rbd/helm/ deploy/rbd/ceph-csi-rbd
-	pushd ../csi-ceph/docs/ > /dev/null
+	pushd ../csi-charts/docs/ > /dev/null
         helm package ../ceph-csi/deploy/rbd/ceph-csi-rbd
         popd > /dev/null
     fi
     popd > /dev/null
 
     if [ $CHANGED -eq 1 ]; then
-        pushd csi-ceph/docs > /dev/null
+        pushd csi-charts/docs > /dev/null
         helm repo index .
         git add --all :/ && git commit -m "Update repo"
         git push https://"$GITHUB_TOKEN"@github.com/ceph/csi-charts
