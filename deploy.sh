96bb1cc625e59cf0f4ccee7b09509f026630c1b0
Madhu Rajanna <madhupr007@gmail.com>
2019-03-21 11:26:49 +0530

    update travis.yml and deploy.sh
    updated travis and deploy to have new branch
    name
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    docker login -u "${QUAY_IO_USERNAME}" -p "${QUAY_IO_PASSWORD}" quay.io 
+if [ "${TRAVIS_BRANCH}" == "csi-v0.3" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    docker login -u "${QUAY_IO_USERNAME}" -p "${QUAY_IO_PASSWORD}" quay.io
     make push-image-rbdplugin push-image-cephfsplugin
 fi
