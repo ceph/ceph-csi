cbd42a21815d202856e2b785f4fd603cddca6b22
Madhu Rajanna <madhupr007@gmail.com>
2019-09-18 17:06:36 +0530

    use rook v1.1.0 in E2E testing
    rook master is not good enough to
    depend on for E2E testing, swithing to v1.1.0
    for E2E
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>
    (cherry picked from commit 18151e9ca8d288fcf80c5ee91a22b683c5178c41)

--- scripts/travis-functest.sh
+++ scripts/travis-functest.sh
@@ -11,6 +11,6 @@ sudo scripts/minikube.sh k8s-sidecar
 sudo chown -R travis: "$HOME"/.minikube /usr/local/bin/kubectl
 # functional tests
 
-go test github.com/ceph/ceph-csi/e2e --rook-version=master --deploy-rook=true --deploy-timeout=10 -timeout=30m -v
+go test github.com/ceph/ceph-csi/e2e --rook-version=v1.1.0 --deploy-rook=true --deploy-timeout=10 -timeout=30m -v
 
 sudo scripts/minikube.sh clean
