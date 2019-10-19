4d7957bca9db2ccefffe275cf6e8a76042fe7f88
Madhu Rajanna <madhupr007@gmail.com>
2019-10-19 10:19:18 +0530

    update snapshot sidecar image in minikube.sh
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -106,7 +106,7 @@ cephcsi)
 k8s-sidecar)
     echo "copying the kubernetes sidecar images"
     copy_image_to_cluster "${K8S_IMAGE_REPO}"/csi-attacher:v1.2.0 "${K8S_IMAGE_REPO}"/csi-attacher:v1.2.0
-    copy_image_to_cluster "${K8S_IMAGE_REPO}"/csi-snapshotter:v1.1.0 $"${K8S_IMAGE_REPO}"/csi-snapshotter:v1.2.1
+    copy_image_to_cluster "${K8S_IMAGE_REPO}"/csi-snapshotter:v1.2.1 $"${K8S_IMAGE_REPO}"/csi-snapshotter:v1.2.1
     copy_image_to_cluster "${K8S_IMAGE_REPO}"/csi-provisioner:v1.3.0 "${K8S_IMAGE_REPO}"/csi-provisioner:v1.3.0
     copy_image_to_cluster "${K8S_IMAGE_REPO}"/csi-node-driver-registrar:v1.1.0 "${K8S_IMAGE_REPO}"/csi-node-driver-registrar:v1.1.0
     ;;
