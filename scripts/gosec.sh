55c60b45d2352c62d274625e1dda50577f373e0a
Niels de Vos <ndevos@redhat.com>
2020-03-24 11:27:01 +0100

    tests: gosec does not support '-mod=vendor'
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- scripts/gosec.sh
+++ scripts/gosec.sh
@@ -3,6 +3,9 @@
 set -o pipefail
 
 if [[ -x "$(command -v gosec)" ]]; then
+  # gosec does not support -mod=vendor, so fallback to non-module support and
+  # assume all dependencies are available in ./vendor already
+  export GO111MODULE=off
   find cmd pkg -type d -print0 | xargs --null gosec
 else
   echo "WARNING: gosec not found, skipping security tests" >&2
