6fa647b7867d62da8504408bd90ef78f304017e3
Niels de Vos <ndevos@redhat.com>
2020-04-24 11:27:12 +0200

    add build step logging to multi-arch build
    With extra logging, there is no need to call `travis_wait` anymore. In
    addition the `travis_wait` command blocks output, so the build steps are
    not reported until the script finishes.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>
    (cherry picked from commit 99dabca319721305c3ae791f15972cacf04c332f)

--- scripts/build-multi-arch-image.sh
+++ scripts/build-multi-arch-image.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# shellcheck source=scripts/build_step.inc.sh
+source "$(dirname "${0}")/build_step.inc.sh"
+
 set -xe
 # "docker manifest" requires experimental feature enabled
 export DOCKER_CLI_EXPERIMENTAL=enabled
@@ -21,6 +25,7 @@ baseimg=$(awk -F = '/^ARG BASE_IMAGE=/ {print $NF}' "${dockerfile}")
 manifests=$(docker manifest inspect "${baseimg}" | jq '.manifests[] | {arch: .platform.architecture, digest: .digest}')
 # qemu-user-static is to enable an execution of different multi-architecture containers by QEMU
 # more info at https://github.com/multiarch/qemu-user-static
+build_step "starting multiarch/qemu-user-static container"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 # build and push per arch images
 for ARCH in amd64 arm64; do
@@ -29,5 +34,6 @@ for ARCH in amd64 arm64; do
 	digest=$(awk -v ARCH=${ARCH} '{if (archfound) {print $NF; exit 0}}; {archfound=($0 ~ "arch.*"ARCH)}' <<<"${manifests}")
 	IFS=$ifs
 	base_img=${baseimg}@${digest}
+	build_step "make image-cephcsi for ${ARCH}"
 	GOARCH=${ARCH} BASE_IMAGE=${base_img} make image-cephcsi
 done
