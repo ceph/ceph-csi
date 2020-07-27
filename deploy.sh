c04350aa56603985b9362f1165764ee86133e490
Madhu Rajanna <madhupr007@gmail.com>
2020-07-27 11:59:44 +0530

    ci: update Makefile and deploy.sh for image tag
    as we need to build and push the v3.0.0 image,
    updated the Makefile and deploy.sh to build and
    push image and helm tempaltes
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -82,8 +82,8 @@ build_push_images() {
 	make push-manifest
 }
 
-if [ "${TRAVIS_BRANCH}" == 'master' ]; then
-	export ENV_CSI_IMAGE_VERSION='canary'
+if [ "${TRAVIS_BRANCH}" == 'release-v3.0' ]; then
+	export ENV_CSI_IMAGE_VERSION='v3.0.0'
 else
 	echo "!!! Branch ${TRAVIS_BRANCH} is not a deployable branch; exiting"
 	exit 0 # Exiting 0 so that this isn't marked as failing
