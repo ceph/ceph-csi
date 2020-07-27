0b6950e738f8bf13456dc0d89ee7b739bcc4d4d4
Madhu Rajanna <madhupr007@gmail.com>
2020-07-27 12:05:50 +0530

    build: update templates for v3.0.0 release
    updated required templates for v3.0.0 release.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -184,7 +184,7 @@ teardown-rook)
     ;;
 cephcsi)
     echo "copying the cephcsi image"
-    copy_image_to_cluster "${CEPHCSI_IMAGE_REPO}"/cephcsi:canary "${CEPHCSI_IMAGE_REPO}"/cephcsi:canary
+    copy_image_to_cluster "${CEPHCSI_IMAGE_REPO}"/cephcsi:v3.0.0 "${CEPHCSI_IMAGE_REPO}"/cephcsi:v3.0.0
     ;;
 k8s-sidecar)
     echo "copying the kubernetes sidecar images"
