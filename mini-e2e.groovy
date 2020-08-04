0449262a13952924b6afdd44e23a1a7e6b7c3a34
Yug <yuggupta27@gmail.com>
2020-08-04 09:17:57 +0530

    ci: Correct the stage name
    Curently the stage name directly
    prints the name of the variable
    in place of substituting it.
    This is a fix for that issue.
    
    Signed-off-by: Yug <yuggupta27@gmail.com>

--- mini-e2e.groovy
+++ mini-e2e.groovy
@@ -46,7 +46,7 @@ node('cico-workspace') {
 			// build e2e.test executable
 			ssh 'cd /opt/build/go/src/github.com/ceph/ceph-csi && make containerized-build CONTAINER_CMD=podman TARGET=e2e.test'
 		}
-		stage("deploy k8s v{k8s_version} and rook") {
+		stage("deploy k8s v${k8s_version} and rook") {
 			timeout(time: 30, unit: 'MINUTES') {
 				ssh "./single-node-k8s.sh --k8s-version=v${k8s_version}"
 			}
