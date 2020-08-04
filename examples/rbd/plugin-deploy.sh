2e14116ed78c74fdee629a48df50637af33f6d58
Yug <yuggupta27@gmail.com>
2020-08-04 18:18:55 +0530

    deploy: add vault creation to rbd driver deployment
    Currently, the script does not deploy the driver singlehandedly;
    As the vault creation needs to be done prior to that.
    The script now includes the vault creation so that
    one script can be sufficient to deploy the rbd driver.
    
    Signed-off-by: Yug <yuggupta27@gmail.com>

--- examples/rbd/plugin-deploy.sh
+++ examples/rbd/plugin-deploy.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 deployment_base="${1}"
+shift
+kms_base="${1}"
 
-if [[ -z $deployment_base ]]; then
+if [[ -z "${deployment_base}" ]]; then
 	deployment_base="../../deploy/rbd/kubernetes"
 fi
 
-cd "$deployment_base" || exit 1
+pushd "${deployment_base}" >/dev/null || exit 1
 
 objects=(csi-provisioner-rbac csi-nodeplugin-rbac csi-config-map csi-rbdplugin-provisioner csi-rbdplugin)
 
 for obj in "${objects[@]}"; do
-	kubectl create -f "./$obj.yaml"
+	kubectl create -f "./${obj}.yaml"
 done
+
+popd >/dev/null || exit 1
+
+if [[ -z "${kms_base}" ]]; then
+	kms_base="../kms/vault"
+fi
+
+pushd "${kms_base}" >/dev/null || exit 1
+
+objects=(vault csi-vaulttokenreview-rbac kms-config)
+
+for obj in "${objects[@]}"; do
+	kubectl create -f "./${obj}.yaml"
+done
+
+popd >/dev/null || exit 1
