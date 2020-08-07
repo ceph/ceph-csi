3dd00e60e74e18f64b50c0f0ce73939804a1ca86
Niels de Vos <ndevos@redhat.com>
2020-08-07 10:18:00 +0200

    util: use local FatalLog() for NewK8sClient() instead of klog
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- internal/util/k8s.go
+++ internal/util/k8s.go
@@ -22,7 +22,6 @@ import (
 	k8s "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	klog "k8s.io/klog/v2"
 )
 
 // NewK8sClient create kubernetes client.
@@ -33,20 +32,17 @@ func NewK8sClient() *k8s.Clientset {
 	if cPath != "" {
 		cfg, err = clientcmd.BuildConfigFromFlags("", cPath)
 		if err != nil {
-			klog.Errorf("Failed to get cluster config with error: %v\n", err)
-			os.Exit(1)
+			FatalLog("Failed to get cluster config with error: %v\n", err)
 		}
 	} else {
 		cfg, err = rest.InClusterConfig()
 		if err != nil {
-			klog.Errorf("Failed to get cluster config with error: %v\n", err)
-			os.Exit(1)
+			FatalLog("Failed to get cluster config with error: %v\n", err)
 		}
 	}
 	client, err := k8s.NewForConfig(cfg)
 	if err != nil {
-		klog.Errorf("Failed to create client with error: %v\n", err)
-		os.Exit(1)
+		FatalLog("Failed to create client with error: %v\n", err)
 	}
 	return client
 }
