d9a16fb5905ad51079c53c784d2770319f836cb7
Nico Berlee <nico.berlee@on2it.net>
2020-09-20 02:43:26 +0200

    deploy: add pod anti-affinity for provisioner deployments
    The added anti-affinity rules prevent provisioner operators from scheduling on
    the same nodes. The kubernetes scheduler will spread the pods across nodes to
    improve availability during node failures.
    
    Signed-off-by: Nico Berlee <nico.berlee@on2it.net>

--- e2e/rbd.go
+++ e2e/rbd.go
@@ -70,6 +70,7 @@ func createORDeleteRbdResouces(action string) {
 	if err != nil {
 		e2elog.Failf("failed to read content from %s with error %v", rbdDirPath+rbdProvisioner, err)
 	}
+	data = oneReplicaDeployYaml(data)
 	_, err = framework.RunKubectlInput(cephCSINamespace, data, action, ns, "-f", "-")
 	if err != nil {
 		e2elog.Failf("failed to %s rbd provisioner with error %v", action, err)
