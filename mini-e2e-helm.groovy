ceph/ceph-csi

commit e0d49908f89af6d33d09963388c41f96cf0a741a

ci: fetch GITHUB_API_TOKEN from Jenkins credential store
Fetch the named credential "github-api-token" from the Jenkins
configuration. This is a "personal access token" that has been created
with the ceph-csi-bot user account.

CC: @ceph-csi-bot
Signed-off-by: Niels de Vos <ndevos@redhat.com>

mini-e2e-helm.groovy
@@ -28,6 +28,10 @@ node('cico-workspace') {
 			skip_e2e = 1
 			return
 		}
+
+		// "github-api-token" is a secret text credential configured in Jenkins
+		env.GITHUB_API_TOKEN = credentials("github-api-token")
+
 		skip_e2e = sh(
 			script: "./scripts/get_github_labels.py --id=${ghprbPullId} --has-label=ci/skip/e2e",
 			returnStatus: true)

mini-e2e.groovy
@@ -25,6 +25,10 @@ node('cico-workspace') {
 			skip_e2e = 1
 			return
 		}
+
+		// "github-api-token" is a secret text credential configured in Jenkins
+		env.GITHUB_API_TOKEN = credentials("github-api-token")
+
 		skip_e2e = sh(
 			script: "./scripts/get_github_labels.py --id=${ghprbPullId} --has-label=ci/skip/e2e",
 			returnStatus: true)

upgrade-tests.groovy
@@ -25,6 +25,10 @@ node('cico-workspace') {
 			skip_e2e = 1
 			return
 		}
+
+		// "github-api-token" is a secret text credential configured in Jenkins
+		env.GITHUB_API_TOKEN = credentials("github-api-token")
+
 		skip_e2e = sh(
 			script: "./scripts/get_github_labels.py --id=${ghprbPullId} --has-label=ci/skip/e2e",
 			returnStatus: true)

commit 626c11cbf4674423f75d34332402d088cc01bbbd

ci: use environment{} to fetch the GitHub API token
The `credentials()` function might only work in the `environment` block
in the Pipelines. At the moment, running the 'skip ci/skip/e2e label'
stage always reports 'Error: 401 Client Error: Unauthorized'.

Fixes: e0d49908 (ci: fetch GITHUB_API_TOKEN from Jenkins credential store)
Signed-off-by: Niels de Vos <ndevos@redhat.com>

mini-e2e-helm.groovy
@@ -24,13 +24,15 @@ node('cico-workspace') {
 	}
 
 	stage('skip ci/skip/e2e label') {
+		environment {
+			// "github-api-token" is a secret text credential configured in Jenkins
+			GITHUB_API_TOKEN = credentials("github-api-token")
+		}
+
 		if (params.ghprbPullId == null) {
 			skip_e2e = 1
 			return
 		}
-
-		// "github-api-token" is a secret text credential configured in Jenkins
-		env.GITHUB_API_TOKEN = credentials("github-api-token")
 
 		skip_e2e = sh(
 			script: "./scripts/get_github_labels.py --id=${ghprbPullId} --has-label=ci/skip/e2e",

mini-e2e.groovy
@@ -21,13 +21,15 @@ node('cico-workspace') {
 	}
 
 	stage('skip ci/skip/e2e label') {
+		environment {
+			// "github-api-token" is a secret text credential configured in Jenkins
+			GITHUB_API_TOKEN = credentials("github-api-token")
+		}
+
 		if (params.ghprbPullId == null) {
 			skip_e2e = 1
 			return
 		}
-
-		// "github-api-token" is a secret text credential configured in Jenkins
-		env.GITHUB_API_TOKEN = credentials("github-api-token")
 
 		skip_e2e = sh(
 			script: "./scripts/get_github_labels.py --id=${ghprbPullId} --has-label=ci/skip/e2e",

upgrade-tests.groovy
@@ -21,13 +21,15 @@ node('cico-workspace') {
 	}
 
 	stage('skip ci/skip/e2e label') {
+		environment {
+			// "github-api-token" is a secret text credential configured in Jenkins
+			GITHUB_API_TOKEN = credentials("github-api-token")
+		}
+
 		if (params.ghprbPullId == null) {
 			skip_e2e = 1
 			return
 		}
-
-		// "github-api-token" is a secret text credential configured in Jenkins
-		env.GITHUB_API_TOKEN = credentials("github-api-token")
 
 		skip_e2e = sh(
 			script: "./scripts/get_github_labels.py --id=${ghprbPullId} --has-label=ci/skip/e2e",