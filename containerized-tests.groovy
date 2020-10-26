466ada38607760c08f18b4899378eef145114db2
Niels de Vos <ndevos@redhat.com>
2020-10-26 08:02:26 +0100

    ci: use github/refs/pull/<pr-id>/merge instead of doing a rebase
    When fetching refs/pull/<pr-id/merge from GitHub, there is no need to do
    a manual rebase. This makes things easier, as a the scripted rebases
    sometimes cause CI jobs to fail.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- containerized-tests.groovy
+++ containerized-tests.groovy
@@ -18,7 +18,7 @@ node('cico-workspace') {
 
 	stage('checkout PR') {
 		if (params.ghprbPullId != null) {
-			ref = "pull/${ghprbPullId}/head"
+			ref = "pull/${ghprbPullId}/merge"
 		}
 		if (params.ghprbTargetBranch != null) {
 			git_since = "${ghprbTargetBranch}"
