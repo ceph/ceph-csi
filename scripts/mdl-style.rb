523d813b4eec869c829437388f5ebcca61d699f5
Niels de Vos <ndevos@redhat.com>
2020-10-28 10:54:56 +0100

    doc: allow in-line HTML in MarkDown documents
    The GitHub style for Pull Request and Issue templates add HTML tags for
    some advanced usage. The MarkDown linter should not give warnings when
    these are used.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- scripts/mdl-style.rb
+++ scripts/mdl-style.rb
@@ -5,5 +5,6 @@ all
 
 rule 'MD013', :code_blocks => false, :tables => false
 
+exclude_rule 'MD033' # In-line HTML: GitHub style markdown adds HTML tags
 exclude_rule 'MD040' # Fenced code blocks should have a language specified
 exclude_rule 'MD041' # First line in file should be a top level header
