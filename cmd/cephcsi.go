bceecd4cb639a78666e4312c783041339bac3987
Madhu Rajanna <madhupr007@gmail.com>
2020-11-17 09:04:29 +0530

    rbd: add minsnapshotsonimage flag
    An rbd image can have a maximum number of
    snapshots defined by maxsnapshotsonimage
    On the limit is reached the cephcsi will
    start flattening the older snapshots and
    returns the ABORT error message, The Request
    comes after this as to wait till all the
    images are flattened (this will increase the
    PVC creation time.  Instead of waiting till
    the maximum snapshots on an RBD image, we can
    have a soft limit, once the limit reached
    cephcsi will start flattening the task to
    break the chain. With this PVC  creation time
    will only be affected when the hard limit
    (minsnapshotsonimage) reached.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- cmd/cephcsi.go
+++ cmd/cephcsi.go
@@ -78,6 +78,7 @@ func init() {
 	flag.UintVar(&conf.RbdHardMaxCloneDepth, "rbdhardmaxclonedepth", 8, "Hard limit for maximum number of nested volume clones that are taken before a flatten occurs")
 	flag.UintVar(&conf.RbdSoftMaxCloneDepth, "rbdsoftmaxclonedepth", 4, "Soft limit for maximum number of nested volume clones that are taken before a flatten occurs")
 	flag.UintVar(&conf.MaxSnapshotsOnImage, "maxsnapshotsonimage", 450, "Maximum number of snapshots allowed on rbd image without flattening")
+	flag.UintVar(&conf.MinSnapshotsOnImage, "minsnapshotsonimage", 250, "Minimum number of snapshots required on rbd image to start flattening")
 	flag.BoolVar(&conf.SkipForceFlatten, "skipforceflatten", false,
 		"skip image flattening if kernel support mapping of rbd images which has the deep-flatten feature")
 
@@ -203,6 +204,10 @@ func validateMaxSnaphostFlag(conf *util.Config) {
 	// [2] https://github.com/torvalds/linux/blob/master/drivers/block/rbd.c#L92
 	if conf.MaxSnapshotsOnImage == 0 || conf.MaxSnapshotsOnImage > 500 {
 		logAndExit("maxsnapshotsonimage flag value should be between 1 and 500")
+	}
+
+	if conf.MinSnapshotsOnImage > conf.MaxSnapshotsOnImage {
+		logAndExit("minsnapshotsonimage flag value should be less than maxsnapshotsonimage")
 	}
 }
 
