ceph/ceph-csi

commit 1eec3792ec0fc834c3cef1838684b287cb55edf0

ci: pre-pull Ceph base-image and cephcsi:devel for mini-e2e-helm jobs
The same changes have been made for the mini-e2e jobs yesterday, and
those seem to work well. Use the same pre-pull method for the Helm
deployment.

Signed-off-by: Niels de Vos <ndevos@redhat.com>

mini-e2e-helm.groovy
@@ -8,12 +8,21 @@ def git_since = "master"
 def skip_e2e = 0
 def doc_change = 0
 def k8s_release = 'latest'
+def ci_registry = 'registry-ceph-csi.apps.ocp.ci.centos.org'
 def namespace = 'cephcsi-e2e-' + UUID.randomUUID().toString().split('-')[-1]
 
 // ssh executes a given command on the reserved bare-metal machine
 // NOTE: do not pass " symbols on the command line, use ' only.
 def ssh(cmd) {
 	sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} \"${cmd}\""
+}
+
+def podman_login(username, passwd) {
+	ssh "podman login --authfile=~/.podman-auth.json --username=${username} --password='${passwd}' ${ci_registry}"
+}
+
+def podman_pull(image) {
+	ssh "podman pull --authfile=~/.podman-auth.json ${ci_registry}/${image} && podman tag ${ci_registry}/${image} ${image}"
 }
 
 node('cico-workspace') {
@@ -96,6 +105,21 @@ node('cico-workspace') {
 			}
 			sh 'scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./prepare.sh ./single-node-k8s.sh root@${CICO_NODE}:'
 			ssh "./prepare.sh --workdir=/opt/build/go/src/github.com/ceph/ceph-csi --gitrepo=${git_repo} --ref=${ref}"
+		}
+		stage('pull base container images') {
+			def base_image = sh(
+				script: 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} \'source /opt/build/go/src/github.com/ceph/ceph-csi/build.env && echo ${BASE_IMAGE}\'',
+				returnStdout: true
+			).trim()
+
+			withCredentials([usernamePassword(credentialsId: 'container-registry-auth', usernameVariable: 'CREDS_USER', passwordVariable: 'CREDS_PASSWD')]) {
+				podman_login("${CREDS_USER}", "${CREDS_PASSWD}:)
+			}
+
+			// base_image is like ceph/ceph:v15
+			podman_pull("${base_image}")
+			// cephcsi:devel is used with 'make containerized-build'
+			podman_pull("ceph-csi:devel")
 		}
 		stage('build artifacts') {
 			// build container image

commit 4b20544a49c506c7b6dae96640ca502f05f830ca

ci: fix calling podman_login()
A typo when calling podman_log() causes CI jobs to fail.

Fixes: 1eec379 "ci: pre-pull Ceph base-image and cephcsi:devel for mini-e2e-helm jobs"
Signed-off-by: Niels de Vos <ndevos@redhat.com>

mini-e2e-helm.groovy
@@ -113,7 +113,7 @@ node('cico-workspace') {
 			).trim()
 
 			withCredentials([usernamePassword(credentialsId: 'container-registry-auth', usernameVariable: 'CREDS_USER', passwordVariable: 'CREDS_PASSWD')]) {
-				podman_login("${CREDS_USER}", "${CREDS_PASSWD}:)
+				podman_login("${CREDS_USER}", "${CREDS_PASSWD}")
 			}
 
 			// base_image is like ceph/ceph:v15

mini-e2e.groovy
@@ -110,7 +110,7 @@ node('cico-workspace') {
 			).trim()
 
 			withCredentials([usernamePassword(credentialsId: 'container-registry-auth', usernameVariable: 'CREDS_USER', passwordVariable: 'CREDS_PASSWD')]) {
-				podman_login("${CREDS_USER}", "${CREDS_PASSWD}:)
+				podman_login("${CREDS_USER}", "${CREDS_PASSWD}")
 			}
 
 			// base_image is like ceph/ceph:v15

containerized-tests.groovy
@@ -95,7 +95,7 @@ node('cico-workspace') {
 				returnStatus: true)
 
 			withCredentials([usernamePassword(credentialsId: 'container-registry-auth', usernameVariable: 'CREDS_USER', passwordVariable: 'CREDS_PASSWD')]) {
-				podman_login("${CREDS_USER}", "${CREDS_PASSWD}:)
+				podman_login("${CREDS_USER}", "${CREDS_PASSWD}")
 			}
 
 			parallel test: {

upgrade-tests.groovy
@@ -110,7 +110,7 @@ node('cico-workspace') {
 			).trim()
 
 			withCredentials([usernamePassword(credentialsId: 'container-registry-auth', usernameVariable: 'CREDS_USER', passwordVariable: 'CREDS_PASSWD')]) {
-				podman_login("${CREDS_USER}", "${CREDS_PASSWD}:)
+				podman_login("${CREDS_USER}", "${CREDS_PASSWD}")
 			}
 
 			// base_image is like ceph/ceph:v15