2e873880849497afb30835fe94c420b8b103f135
Madhu Rajanna <madhupr007@gmail.com>
2021-02-08 17:55:18 +0530

    cleanup: move servers to a new struct
    For future enhancments like adding more
    servers. Moving the list of servers to a
    new structure.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- internal/rbd/driver.go
+++ internal/rbd/driver.go
@@ -168,7 +168,12 @@ func (r *Driver) Run(conf *util.Config) {
 	}
 
 	s := csicommon.NewNonBlockingGRPCServer()
-	s.Start(conf.Endpoint, conf.HistogramOption, r.ids, r.cs, r.ns, conf.EnableGRPCMetrics)
+	srv := csicommon.Servers{
+		IS: r.ids,
+		CS: r.cs,
+		NS: r.ns,
+	}
+	s.Start(conf.Endpoint, conf.HistogramOption, srv, conf.EnableGRPCMetrics)
 	if conf.EnableGRPCMetrics {
 		util.WarningLogMsg("EnableGRPCMetrics is deprecated")
 		go util.StartMetricsServer(conf)
