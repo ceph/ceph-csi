5e63743243ca5cd990d773a089d48dd513f388c5
Niels de Vos <ndevos@redhat.com>
2021-03-02 18:48:05 +0100

    util: add SecretsMetadataKMS
    This new KMS is based on the (default) SecretsKMS, but instead of using
    the passphrase for all volumes, the passphrase is used to
    encrypt/decrypt a Data-Encryption-Key that is stored in the metadata of
    the volume.
    
    CC: Patrick Uiterwijk <puiterwijk@redhat.com> - for encryption guidance
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- internal/util/secretskms.go
+++ internal/util/secretskms.go
@@ -17,7 +17,15 @@ limitations under the License.
 package util
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+
+	"golang.org/x/crypto/scrypt"
 )
 
 const (
@@ -26,6 +34,10 @@ const (
 
 	// Default KMS type
 	defaultKMSType = "default"
+
+	// kmsTypeSecretsMetadata is the SecretsKMS with per-volume encryption,
+	// where the DEK is stored in the metadata of the volume itself.
+	kmsTypeSecretsMetadata = "metadata"
 )
 
 // SecretsKMS is default KMS implementation that means no KMS is in use.
@@ -71,4 +83,147 @@ func (kms SecretsKMS) StoreDEK(key, value string) error {
 // SecretsKMS.
 func (kms SecretsKMS) RemoveDEK(key string) error {
 	return nil
+}
+
+// SecretsMetadataKMS is a KMS based on the SecretsKMS, but stores the
+// Data-Encryption-Key (DEK) in the metadata of the volume.
+type SecretsMetadataKMS struct {
+	SecretsKMS
+
+	encryptionKMSID string
+}
+
+// initSecretsMetadataKMS initializes a SecretsMetadataKMS that wraps a
+// SecretsKMS, so that the passphrase from the StorageClass secrets can be used
+// for encrypting/decrypting DEKs that are stored in a detached DEKStore.
+func initSecretsMetadataKMS(encryptionKMSID string, secrets map[string]string) (EncryptionKMS, error) {
+	eKMS, err := initSecretsKMS(secrets)
+	if err != nil {
+		return nil, err
+	}
+
+	sKMS, ok := eKMS.(SecretsKMS)
+	if !ok {
+		return nil, fmt.Errorf("failed to convert %T to SecretsKMS", eKMS)
+	}
+
+	smKMS := SecretsMetadataKMS{}
+	smKMS.SecretsKMS = sKMS
+	smKMS.encryptionKMSID = encryptionKMSID
+
+	return smKMS, nil
+}
+
+// GetID is returning ID representing the SecretsMetadataKMS.
+func (kms SecretsMetadataKMS) GetID() string {
+	return kms.encryptionKMSID
+}
+
+// Destroy frees all used resources.
+func (kms SecretsMetadataKMS) Destroy() {
+	kms.SecretsKMS.Destroy()
+}
+
+func (kms SecretsMetadataKMS) requiresDEKStore() DEKStoreType {
+	return DEKStoreMetadata
+}
+
+// encryptedMetedataDEK contains the encrypted DEK and the Nonce that was used
+// during encryption. This structure is stored (in JSON format) in the DEKStore
+// that is linked to this KMS provider.
+type encryptedMetedataDEK struct {
+	// DEK is the encrypted data-encryption-key for the volume.
+	DEK []byte `json:"dek"`
+	// Nonce is a random byte slice to guarantee the uniqueness of the
+	// encrypted DEK.
+	Nonce []byte `json:"nonce"`
+}
+
+// EncryptDEK encrypts the plainDEK with a key derived from the passphrase from
+// the SecretsKMS and the volumeID.
+// The resulting encryptedDEK contains a JSON with the encrypted DEK and the
+// nonce that was used for encrypting.
+func (kms SecretsMetadataKMS) EncryptDEK(volumeID, plainDEK string) (string, error) {
+	// use the passphrase from the SecretsKMS
+	passphrase, err := kms.SecretsKMS.FetchDEK(volumeID)
+	if err != nil {
+		return "", fmt.Errorf("failed to get passphrase: %w", err)
+	}
+
+	aead, err := generateCipher(passphrase, volumeID)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate cipher: %w", err)
+	}
+
+	emd := encryptedMetedataDEK{}
+	emd.Nonce, err = generateNonce(aead.NonceSize())
+	if err != nil {
+		return "", fmt.Errorf("failed to generated nonce: %w", err)
+	}
+	emd.DEK = aead.Seal(nil, emd.Nonce, []byte(plainDEK), nil)
+
+	emdData, err := json.Marshal(&emd)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert "+
+			"encryptedMetedataDEK to JSON: %w", err)
+	}
+
+	return string(emdData), nil
+}
+
+// DecryptDEK takes the JSON formatted `encryptedMetedataDEK` contents, and it
+// fetches SecretsKMS passphase to decrypt the DEK.
+func (kms SecretsMetadataKMS) DecryptDEK(volumeID, encryptedDEK string) (string, error) {
+	// use the passphrase from the SecretsKMS
+	passphrase, err := kms.SecretsKMS.FetchDEK(volumeID)
+	if err != nil {
+		return "", fmt.Errorf("failed to get passphrase: %w", err)
+	}
+
+	aead, err := generateCipher(passphrase, volumeID)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate cipher: %w", err)
+	}
+
+	emd := encryptedMetedataDEK{}
+	err = json.Unmarshal([]byte(encryptedDEK), &emd)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert data to "+
+			"encryptedMetedataDEK: %w", err)
+	}
+
+	dek, err := aead.Open(nil, emd.Nonce, emd.DEK, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to decrypt DEK: %w", err)
+	}
+
+	return string(dek), nil
+}
+
+// generateCipher returns a AEAD cipher based on a passphrase and salt
+// (volumeID). The cipher can then be used to encrypt/decrypt the DEK.
+func generateCipher(passphrase, salt string) (cipher.AEAD, error) {
+	key, err := scrypt.Key([]byte(passphrase), []byte(salt), 32768, 8, 1, 32)
+	if err != nil {
+		return nil, err
+	}
+	blockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	aead, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		return nil, err
+	}
+	return aead, nil
+}
+
+// generateNonce returns a byte slice with random contents.
+func generateNonce(size int) ([]byte, error) {
+	nonce := make([]byte, size)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return nonce, nil
 }
