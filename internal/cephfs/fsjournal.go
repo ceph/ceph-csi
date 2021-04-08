9555e6391f42cbb36b9c5129eebf898ed631fdcf
Rakshith R <rar@redhat.com>
2021-04-08 16:59:12 +0530

    cleanup: correct nolint directive listing format
    nolint directive needs to be followed by comma separated
    list of linters. This commit changes to gocognit:gocyclo
    which was not recognised to linters which show error for
    the function.
    
    Signed-off-by: Rakshith R <rar@redhat.com>

--- internal/cephfs/fsjournal.go
+++ internal/cephfs/fsjournal.go
@@ -55,7 +55,7 @@ because, the order of omap creation and deletion are inverse of each other, and 
 request name lock, and hence any stale omaps are leftovers from incomplete transactions and are
 hence safe to garbage collect.
 */
-// nolint:gocognit:gocyclo // TODO: reduce complexity
+// nolint:gocognit,gocyclo,nestif // TODO: reduce complexity
 func checkVolExists(ctx context.Context,
 	volOptions,
 	parentVolOpt *volumeOptions,
