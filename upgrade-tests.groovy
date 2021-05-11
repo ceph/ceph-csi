ceph/ceph-csi

commit e36155283b6747dc766ca56fc86d205ec5ba5718

ci: run system-status.sh in case a job fails
The new `system-status.sh` script logs the status of the host and the
minikube VM. This gets executed when a CI job fails, and should aid in
troubleshooting spurious failures.

Updates: #1969
Signed-off-by: Niels de Vos <ndevos@redhat.com>

k8s-e2e-external-storage.groovy
@@ -181,6 +181,12 @@ node('cico-workspace') {
 		}
 	}
 
+	catch (err) {
+		stage('log system status') {
+			ssh './system-status.sh'
+		}
+	}
+
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'

mini-e2e-helm.groovy
@@ -188,6 +188,12 @@ node('cico-workspace') {
 		}
 	}
 
+	catch (err) {
+		stage('log system status') {
+			ssh './system-status.sh'
+		}
+	}
+
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'

mini-e2e.groovy
@@ -171,6 +171,12 @@ node('cico-workspace') {
 		}
 	}
 
+	catch (err) {
+		stage('log system status') {
+			ssh './system-status.sh'
+		}
+	}
+
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'

upgrade-tests.groovy
@@ -175,6 +175,12 @@ node('cico-workspace') {
 		}
 	}
 
+	catch (err) {
+		stage('log system status') {
+			ssh './system-status.sh'
+		}
+	}
+
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'

system-status.sh
@@ -1,0 +1,40 @@
+#!/bin/bash
+#
+# Run this script to gather details about the environment where the CI job is
+# running. This can be helpful to identify issues why minikube failed to
+# deploy, or tests encounter problems while running.
+#
+
+function minikube_ssh() {
+    ssh \
+        -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+        -l docker -i "$(minikube ssh-key)" \
+        "$(minikube ip)" "${*}"
+}
+
+function log() {
+    echo "###"
+    echo "### going to execute: ${*}"
+    echo "###"
+    "${@}"
+    echo "###"
+    echo "### execution finished: ${*}"
+    echo "###"
+}
+
+# get the status of the VM in libvirt
+log virsh list
+
+# status of the minikube Kubernetes cluster
+log minikube status
+log minikube logs
+
+# get the status of processes in the VM
+log minikube_ssh top -b -c -n1
+
+# get the logs from the VM
+log minikube_ssh journalctl --boot
+
+# filesystem status for host and VM
+log df -h
+log minikube_ssh df -h

commit 72838619be75d34120e0e1c59529ed07cb32b338

ci: in case of a failure, return error after logging system status
It seems that it is required to re-throw the error after a catch{..}
block. Without this, and a successful execution of system-status.sh, the
CI jobs get marked as SUCCESS, even when there was a failure.

Fixes: e36155283 "ci: run system-status.sh in case a job fails"
Signed-off-by: Niels de Vos <ndevos@redhat.com>

k8s-e2e-external-storage.groovy
@@ -10,6 +10,7 @@ def doc_change = 0
 def k8s_release = 'latest'
 def namespace = 'k8s-e2e-storage-' + UUID.randomUUID().toString().split('-')[-1]
 def ci_registry = 'registry-ceph-csi.apps.ocp.ci.centos.org'
+def failure = null
 
 def ssh(cmd) {
 	sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} '${cmd}'"
@@ -182,6 +183,8 @@ node('cico-workspace') {
 	}
 
 	catch (err) {
+		failure = err
+
 		stage('log system status') {
 			ssh './system-status.sh'
 		}
@@ -190,6 +193,10 @@ node('cico-workspace') {
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'
+		}
+
+		if (failure) {
+			throw failure
 		}
 	}
 }

mini-e2e-helm.groovy
@@ -10,6 +10,7 @@ def doc_change = 0
 def k8s_release = 'latest'
 def ci_registry = 'registry-ceph-csi.apps.ocp.ci.centos.org'
 def namespace = 'cephcsi-e2e-' + UUID.randomUUID().toString().split('-')[-1]
+def failure = null
 
 // ssh executes a given command on the reserved bare-metal machine
 // NOTE: do not pass " symbols on the command line, use ' only.
@@ -189,6 +190,8 @@ node('cico-workspace') {
 	}
 
 	catch (err) {
+		failure = err
+
 		stage('log system status') {
 			ssh './system-status.sh'
 		}
@@ -197,6 +200,10 @@ node('cico-workspace') {
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'
+		}
+
+		if (failure) {
+			throw failure
 		}
 	}
 }

mini-e2e.groovy
@@ -9,6 +9,7 @@ def skip_e2e = 0
 def doc_change = 0
 def k8s_release = 'latest'
 def ci_registry = 'registry-ceph-csi.apps.ocp.ci.centos.org'
+def failure = null
 
 def ssh(cmd) {
 	sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} '${cmd}'"
@@ -172,6 +173,8 @@ node('cico-workspace') {
 	}
 
 	catch (err) {
+		failure = err
+
 		stage('log system status') {
 			ssh './system-status.sh'
 		}
@@ -180,6 +183,10 @@ node('cico-workspace') {
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'
+		}
+
+		if (failure) {
+			throw failure
 		}
 	}
 }

upgrade-tests.groovy
@@ -9,6 +9,7 @@ def skip_e2e = 0
 def doc_change = 0
 def k8s_release = 'latest'
 def ci_registry = 'registry-ceph-csi.apps.ocp.ci.centos.org'
+def failure = null
 
 def ssh(cmd) {
 	sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} '${cmd}'"
@@ -176,6 +177,8 @@ node('cico-workspace') {
 	}
 
 	catch (err) {
+		failure = err
+
 		stage('log system status') {
 			ssh './system-status.sh'
 		}
@@ -184,6 +187,10 @@ node('cico-workspace') {
 	finally {
 		stage('return bare-metal machine') {
 			sh 'cico node done ${CICO_SSID}'
+		}
+
+		if (failure) {
+			throw failure
 		}
 	}
 }