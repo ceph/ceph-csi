96fce60b4eebeb77fd708bab6af88d8aceb09def
Madhu Rajanna <madhupr007@gmail.com>
2021-06-17 11:45:29 +0530

    rbd: add validation for thick restore/clone
    added validation to allow only Restore of Thick PVC
    snapshot to a thick clone and creation of thick clone
    from thick PVC.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>
    (cherry picked from commit 7966d2e5c127b2d716732fa7cacf2b90d5921e9a)

--- internal/rbd/rbd_util.go
+++ internal/rbd/rbd_util.go
@@ -1550,3 +1550,19 @@ func (ri *rbdImage) isCompatibleEncryption(dst *rbdImage) error {
 
 	return nil
 }
+
+func (ri *rbdImage) isCompatibleThickProvision(dst *rbdVolume) error {
+	thick, err := ri.isThickProvisioned()
+	if err != nil {
+		return err
+	}
+	switch {
+	case thick && !dst.ThickProvision:
+		return fmt.Errorf("cannot create thin volume from thick volume %q", ri)
+
+	case !thick && dst.ThickProvision:
+		return fmt.Errorf("cannot create thick volume from thin volume %q", ri)
+	}
+
+	return nil
+}
