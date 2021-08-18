62666bd6f1ca0364a1e6e6ed30c4fd3909144ed1
Madhu Rajanna <madhupr007@gmail.com>
2021-08-18 17:19:18 +0530

    ci: tag ceph image with quay.io
    added if else check to check the
    image starting with quay.io. If yes,
    pull and tag image with quay.io
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- containerized-tests.groovy
+++ containerized-tests.groovy
@@ -137,10 +137,19 @@ node('cico-workspace') {
 						script: 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} \'source /opt/build/go/src/github.com/ceph/ceph-csi/build.env && echo ${BASE_IMAGE}\'',
 						returnStdout: true
 					).trim()
-					def d_io_regex = ~"^docker.io/"
+					def quay = "quay.io"
+					def docker = "docker.io"
 
+					if (base_image.startsWith(quay)){
+					def q_io_regex = ~"^quay.io/"
+					// base_image is like quay.io/ceph/ceph:v15, strip "quay.io/"
+					podman_pull(ci_registry, quay, "${base_image}" - q_io_regex)
+					}else{
+					def d_io_regex = ~"^docker.io/"
 					// base_image is like ceph/ceph:v15 or docker.io/ceph/ceph:v15, strip "docker.io/"
-					podman_pull(ci_registry, "docker.io", "${base_image}" - d_io_regex)
+					podman_pull(ci_registry, docker , "${base_image}" - d_io_regex)
+					}
+
 				}
 			}
 		}
