ad2c6d285135524be2cbcce7b41cd91983fdff3c
Prasanna Kumar Kalever <prasanna.kalever@redhat.com>
2021-09-01 17:33:01 +0530

    util: add gzip helper function
    Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

---
+++ internal/util/log/log_utils_test.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2021 ceph-csi authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package log
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestGzipLogFile(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	logFile, err := ioutil.TempFile(tmpDir, "rbd-*.log")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer os.Remove(logFile.Name())
+
+	if err = GzipLogFile(logFile.Name()); err != nil {
+		t.Errorf("GzipLogFile failed: %v", err)
+	}
+
+	newExt := strings.Replace(logFile.Name(), ".log", ".gz", -1)
+	if _, err = os.Stat(newExt); errors.Is(err, os.ErrNotExist) {
+		t.Errorf("compressed logFile (%s) not found: %v", newExt, err)
+	}
+
+	os.Remove(newExt)
+}
