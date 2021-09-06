73e2ffe8b851929d2cd44d9c132c28574454911e
Madhu Rajanna <madhupr007@gmail.com>
2021-09-06 17:48:53 +0530

    cleanup: move cephfs csi spec validation to validator
    moved the cephfs related validation like
    validating the input parameters sent in the
    GRPC request to a new file.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- internal/cephfs/util.go
+++ internal/cephfs/util.go
@@ -18,7 +18,6 @@ package cephfs
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/ceph/ceph-csi/internal/util"
@@ -27,8 +26,6 @@ import (
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/timestamp"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type volumeID string
@@ -37,91 +34,6 @@ func execCommandErr(ctx context.Context, program string, args ...string) error {
 	_, _, err := util.ExecCommand(ctx, program, args...)
 
 	return err
-}
-
-// Controller service request validation.
-func (cs *ControllerServer) validateCreateVolumeRequest(req *csi.CreateVolumeRequest) error {
-	if err := cs.Driver.ValidateControllerServiceRequest(
-		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME); err != nil {
-		return fmt.Errorf("invalid CreateVolumeRequest: %w", err)
-	}
-
-	if req.GetName() == "" {
-		return status.Error(codes.InvalidArgument, "volume Name cannot be empty")
-	}
-
-	reqCaps := req.GetVolumeCapabilities()
-	if reqCaps == nil {
-		return status.Error(codes.InvalidArgument, "volume Capabilities cannot be empty")
-	}
-
-	for _, capability := range reqCaps {
-		if capability.GetBlock() != nil {
-			return status.Error(codes.Unimplemented, "block volume not supported")
-		}
-	}
-
-	// Allow readonly access mode for volume with content source
-	err := util.CheckReadOnlyManyIsSupported(req)
-	if err != nil {
-		return err
-	}
-
-	if req.VolumeContentSource != nil {
-		volumeSource := req.VolumeContentSource
-		switch volumeSource.Type.(type) {
-		case *csi.VolumeContentSource_Snapshot:
-			snapshot := req.VolumeContentSource.GetSnapshot()
-			// CSI spec requires returning NOT_FOUND when the volumeSource is missing/incorrect.
-			if snapshot == nil {
-				return status.Error(codes.NotFound, "volume Snapshot cannot be empty")
-			}
-			if snapshot.GetSnapshotId() == "" {
-				return status.Error(codes.NotFound, "volume Snapshot ID cannot be empty")
-			}
-		case *csi.VolumeContentSource_Volume:
-			// CSI spec requires returning NOT_FOUND when the volumeSource is missing/incorrect.
-			vol := req.VolumeContentSource.GetVolume()
-			if vol == nil {
-				return status.Error(codes.NotFound, "volume cannot be empty")
-			}
-			if vol.GetVolumeId() == "" {
-				return status.Error(codes.NotFound, "volume ID cannot be empty")
-			}
-
-		default:
-			return status.Error(codes.InvalidArgument, "unsupported volume data source")
-		}
-	}
-
-	return nil
-}
-
-func (cs *ControllerServer) validateDeleteVolumeRequest() error {
-	if err := cs.Driver.ValidateControllerServiceRequest(
-		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME); err != nil {
-		return fmt.Errorf("invalid DeleteVolumeRequest: %w", err)
-	}
-
-	return nil
-}
-
-// Controller expand volume request validation.
-func (cs *ControllerServer) validateExpandVolumeRequest(req *csi.ControllerExpandVolumeRequest) error {
-	if err := cs.Driver.ValidateControllerServiceRequest(csi.ControllerServiceCapability_RPC_EXPAND_VOLUME); err != nil {
-		return fmt.Errorf("invalid ExpandVolumeRequest: %w", err)
-	}
-
-	if req.GetVolumeId() == "" {
-		return status.Error(codes.InvalidArgument, "Volume ID cannot be empty")
-	}
-
-	capRange := req.GetCapacityRange()
-	if capRange == nil {
-		return status.Error(codes.InvalidArgument, "CapacityRange cannot be empty")
-	}
-
-	return nil
 }
 
 func genSnapFromOptions(ctx context.Context, req *csi.CreateSnapshotRequest) (snap *cephfsSnapshot, err error) {
