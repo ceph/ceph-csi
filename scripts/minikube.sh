51075bc0187180280f0331bf99517879f921b371
Rakshith R <rar@redhat.com>
2021-09-08 16:00:11 +0530

    ci: use 0 as default NUM_DISKS in minikube.sh
    This is done to prevent conflicts with current ci setup externally
    attaching disks.
    
    Signed-off-by: Rakshith R <rar@redhat.com>
    (cherry picked from commit 8f75a24cfd17d9f098cd18ccb12630d33523e370)

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -163,7 +163,7 @@ MEMORY=${MEMORY:-"4096"}
 CPUS=${CPUS:-"$(nproc)"}
 VM_DRIVER=${VM_DRIVER:-"virtualbox"}
 CNI=${CNI:-"bridge"}
-NUM_DISKS=${NUM_DISKS:-"1"}
+NUM_DISKS=${NUM_DISKS:-"0"}
 DISK_SIZE=${DISK_SIZE:-"32g"}
 #configure image repo
 CEPHCSI_IMAGE_REPO=${CEPHCSI_IMAGE_REPO:-"quay.io/cephcsi"}
