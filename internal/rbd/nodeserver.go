ceph/ceph-csi

commit 9a3170bf77fd8d7c098c1425bde24690909bdb37

rbd: provide a way to disable the auto fallback to nbd mounter
This change allows the user to choose not to fallback to NBD mounter
when some ImageFeatures are absent with krbd driver, rather just fail
the NodeStage call.

Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

internal/rbd/nodeserver.go
@@ -68,8 +68,9 @@ const (
 	xfsReflinkNoSupport
 	xfsReflinkSupport
 
-	staticVol    = "staticVolume"
-	volHealerCtx = "volumeHealerContext"
+	staticVol        = "staticVolume"
+	volHealerCtx     = "volumeHealerContext"
+	tryOtherMounters = "tryOtherMounters"
 )
 
 var (
@@ -218,6 +219,11 @@ func populateRbdVol(
 	krbdSupported := false
 	if req.GetVolumeContext()["mounter"] != rbdNbdMounter {
 		krbdSupported = isKrbdFeatureSupported(ctx, req.GetVolumeContext()["imageFeatures"])
+		if !krbdSupported && !parseBoolOption(ctx, req.GetVolumeContext(), tryOtherMounters, false) {
+			log.ErrorLog(ctx, "unsupported krbd Feature, set `tryOtherMounters:true` or fix krbd driver")
+
+			return nil, status.Errorf(codes.Internal, "unsupported krbd Feature")
+		}
 	}
 	if krbdSupported {
 		rv.MapOptions = req.GetVolumeContext()["mapOptions"]

charts/ceph-csi-rbd/templates/storageclass.yaml
@@ -19,6 +19,9 @@ parameters:
   pool: {{ .Values.storageClass.pool }}
   imageFeatures: {{ .Values.storageClass.imageFeatures }}
   thickProvision: {{ .Values.storageClass.thickProvision | quote}}
+{{- if .Values.storageClass.tryOtherMounters }}
+  tryOtherMounters: {{ .Values.storageClass.tryOtherMounters | quote}}
+{{- end }}
 {{- if .Values.storageClass.mounter }}
   mounter: {{ .Values.storageClass.mounter }}
 {{- end }}

charts/ceph-csi-rbd/values.yaml
@@ -289,6 +289,15 @@ storageClass:
   # imageFeatures: layering,journaling,exclusive-lock
   imageFeatures: layering
 
+  # (optional) Specifies whether to try other mounters in case if the current
+  # mounter fails to mount the rbd image for any reason. True means fallback
+  # to next mounter, default is set to false.
+  # Note: tryOtherMounters is currently useful to fallback from krbd to rbd-nbd
+  # in case if any of the specified imageFeatures is not supported by krbd
+  # driver on node scheduled for application pod launch, but in the future this
+  # should work with any mounter type.
+  # tryOtherMounters: false
+
   # (optional) uncomment the following to use rbd-nbd as mounter
   # on supported nodes
   # mounter: rbd-nbd

examples/rbd/storageclass.yaml
@@ -39,6 +39,15 @@ parameters:
    # imageFeatures: layering,journaling,exclusive-lock
    imageFeatures: layering
 
+   # (optional) Specifies whether to try other mounters in case if the current
+   # mounter fails to mount the rbd image for any reason. True means fallback
+   # to next mounter, default is set to false.
+   # Note: tryOtherMounters is currently useful to fallback from krbd to rbd-nbd
+   # in case if any of the specified imageFeatures is not supported by krbd
+   # driver on node scheduled for application pod launch, but in the future this
+   # should work with any mounter type.
+   # tryOtherMounters: false
+
    # (optional) mapOptions is a comma-separated list of map options.
    # For krbd options refer
    # https://docs.ceph.com/docs/master/man/8/rbd/#kernel-rbd-krbd-options

commit 46d578982d3df3b7329b9213759dc309b388dfbd

rbd: fix mapOptions passing with rbd-nbd mounter
This was a regression introduced by:
https://github.com/ceph/ceph-csi/pull/2556

Fixes: #2610
Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

internal/rbd/nodeserver.go
@@ -216,23 +216,20 @@ func populateRbdVol(
 		rv.RbdImageName = imageAttributes.ImageName
 	}
 
-	krbdSupported := false
-	if req.GetVolumeContext()["mounter"] != rbdNbdMounter {
-		krbdSupported = isKrbdFeatureSupported(ctx, req.GetVolumeContext()["imageFeatures"])
-		if !krbdSupported && !parseBoolOption(ctx, req.GetVolumeContext(), tryOtherMounters, false) {
+	if req.GetVolumeContext()["mounter"] == rbdDefaultMounter &&
+		!isKrbdFeatureSupported(ctx, req.GetVolumeContext()["imageFeatures"]) {
+		if !parseBoolOption(ctx, req.GetVolumeContext(), tryOtherMounters, false) {
 			log.ErrorLog(ctx, "unsupported krbd Feature, set `tryOtherMounters:true` or fix krbd driver")
 
 			return nil, status.Errorf(codes.Internal, "unsupported krbd Feature")
 		}
-	}
-	if krbdSupported {
-		rv.MapOptions = req.GetVolumeContext()["mapOptions"]
-		rv.UnmapOptions = req.GetVolumeContext()["unmapOptions"]
-		rv.Mounter = req.GetVolumeContext()["mounter"]
-	} else {
 		// fallback to rbd-nbd,
 		// ignore the mapOptions and unmapOptions as they are meant for krbd use.
 		rv.Mounter = rbdNbdMounter
+	} else {
+		rv.Mounter = req.GetVolumeContext()["mounter"]
+		rv.MapOptions = req.GetVolumeContext()["mapOptions"]
+		rv.UnmapOptions = req.GetVolumeContext()["unmapOptions"]
 	}
 
 	rv.VolID = volID