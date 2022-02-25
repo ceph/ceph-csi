6d83df9cc93f5ebe7132ab01db9f537bd12f70ae
Niels de Vos <ndevos@redhat.com>
2022-02-25 13:16:15 +0100

    nfs: add basic provisioner with create/delete procedures
    These NFS Controller and Identity servers are the base for the new
    provisioner. The functionality is currently extremely limited, follow-up
    PRs will implement various CSI procedures.
    
    CreateVolume is implemented with the bare minimum. This makes it
    possible to create a volume, and mount it with the
    kubernetes-csi/csi-driver-nfs NodePlugin.
    
    DeleteVolume unexports the volume from the Ceph managed NFS-Ganesha
    service. In case the Ceph cluster provides multiple NFS-Ganesha
    deployments, things might not work as expected. This is going to be
    addressed in follow-up improvements.
    
    Lots of TODO comments need to be resolved before this can be declared
    "production ready". Unit- and e2e-tests are missing as well.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

---
+++ internal/nfs/driver/driver.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2022 The Ceph-CSI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package driver
+
+import (
+	csicommon "github.com/ceph/ceph-csi/internal/csi-common"
+	"github.com/ceph/ceph-csi/internal/nfs/controller"
+	"github.com/ceph/ceph-csi/internal/nfs/identity"
+	"github.com/ceph/ceph-csi/internal/util"
+	"github.com/ceph/ceph-csi/internal/util/log"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+// Driver contains the default identity and controller struct.
+type Driver struct{}
+
+// NewDriver returns new ceph driver.
+func NewDriver() *Driver {
+	return &Driver{}
+}
+
+// Run start a non-blocking grpc controller,node and identityserver for
+// ceph CSI driver which can serve multiple parallel requests.
+func (fs *Driver) Run(conf *util.Config) {
+	// Initialize default library driver
+	cd := csicommon.NewCSIDriver(conf.DriverName, util.DriverVersion, conf.NodeID)
+	if cd == nil {
+		log.FatalLogMsg("failed to initialize CSI driver")
+	}
+
+	cd.AddControllerServiceCapabilities([]csi.ControllerServiceCapability_RPC_Type{
+		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
+		csi.ControllerServiceCapability_RPC_SINGLE_NODE_MULTI_WRITER,
+	})
+	// VolumeCapabilities are validated by the CephFS Controller
+	cd.AddVolumeCapabilityAccessModes([]csi.VolumeCapability_AccessMode_Mode{
+		csi.VolumeCapability_AccessMode_MULTI_NODE_MULTI_WRITER,
+		csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
+		csi.VolumeCapability_AccessMode_SINGLE_NODE_MULTI_WRITER,
+		csi.VolumeCapability_AccessMode_SINGLE_NODE_SINGLE_WRITER,
+	})
+
+	// Create gRPC servers
+	server := csicommon.NewNonBlockingGRPCServer()
+	srv := csicommon.Servers{
+		IS: identity.NewIdentityServer(cd),
+		CS: controller.NewControllerServer(cd),
+	}
+	server.Start(conf.Endpoint, conf.HistogramOption, srv, conf.EnableGRPCMetrics)
+	if conf.EnableGRPCMetrics {
+		log.WarningLogMsg("EnableGRPCMetrics is deprecated")
+		go util.StartMetricsServer(conf)
+	}
+	if conf.EnableProfiling {
+		if !conf.EnableGRPCMetrics {
+			go util.StartMetricsServer(conf)
+		}
+		log.DebugLogMsg("Registering profiling handler")
+		go util.EnableProfiling()
+	}
+	server.Wait()
+}
---
+++ internal/nfs/identity/identityserver.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2022 The Ceph-CSI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package identity
+
+import (
+	"context"
+
+	csicommon "github.com/ceph/ceph-csi/internal/csi-common"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+// Server struct of ceph CSI driver with supported methods of CSI identity
+// server spec.
+type Server struct {
+	*csicommon.DefaultIdentityServer
+}
+
+// NewIdentityServer initialize a identity server for ceph CSI driver.
+func NewIdentityServer(d *csicommon.CSIDriver) *Server {
+	return &Server{
+		DefaultIdentityServer: csicommon.NewDefaultIdentityServer(d),
+	}
+}
+
+// GetPluginCapabilities returns available capabilities of the ceph driver.
+func (is *Server) GetPluginCapabilities(
+	ctx context.Context,
+	req *csi.GetPluginCapabilitiesRequest) (*csi.GetPluginCapabilitiesResponse, error) {
+	return &csi.GetPluginCapabilitiesResponse{
+		Capabilities: []*csi.PluginCapability{
+			{
+				Type: &csi.PluginCapability_Service_{
+					Service: &csi.PluginCapability_Service{
+						Type: csi.PluginCapability_Service_CONTROLLER_SERVICE,
+					},
+				},
+			},
+		},
+	}, nil
+}
