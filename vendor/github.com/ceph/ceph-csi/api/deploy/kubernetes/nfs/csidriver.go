b6a94561792b0c8f3f83b06e83325a57fd3dab47
Niels de Vos <ndevos@redhat.com>
2022-03-02 09:32:01 +0100

    deploy: add CSIDriver for NFS
    The API is extended for generation of the NFS CSIDriver object. The
    YAML file under deploy/ was created by `yamlgen`.
    
    The contents of the csidriver.yaml file is heavily based on the upstream
    CSIDriver from the Kubernetes csi-driver-nfs project.
    
    Because ./tools/yamlgen uses the API, it gets copied under vendor/ .
    This causes two copies of the API to be included in the repository, but
    that can not be prevented, it seems.
    
    See-also: https://github.com/kubernetes-csi/csi-driver-nfs/blob/master/deploy/csi-nfs-driverinfo.yaml
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

---
+++ vendor/github.com/ceph/ceph-csi/api/deploy/kubernetes/nfs/csidriver.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2022 The Ceph-CSI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package nfs
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"text/template"
+
+	"github.com/ghodss/yaml"
+	storagev1 "k8s.io/api/storage/v1"
+)
+
+//go:embed csidriver.yaml
+var csiDriver string
+
+type CSIDriverValues struct {
+	Name string
+}
+
+var CSIDriverDefaults = CSIDriverValues{
+	Name: "nfs.csi.ceph.com",
+}
+
+// NewCSIDriver takes a driver name from the CSIDriverValues struct and
+// replaces the value in the template. A CSIDriver object is returned which can
+// be created in the Kubernetes cluster.
+func NewCSIDriver(values CSIDriverValues) (*storagev1.CSIDriver, error) {
+	data, err := NewCSIDriverYAML(values)
+	if err != nil {
+		return nil, err
+	}
+
+	driver := &storagev1.CSIDriver{}
+	err = yaml.Unmarshal([]byte(data), driver)
+	if err != nil {
+		return nil, fmt.Errorf("failed convert YAML to %T: %w", driver, err)
+	}
+
+	return driver, nil
+}
+
+// NewCSIDriverYAML takes a driver name from the CSIDriverValues struct and
+// replaces the value in the template. A CSIDriver object in YAML is returned
+// which can be created in the Kubernetes cluster.
+func NewCSIDriverYAML(values CSIDriverValues) (string, error) {
+	var buf bytes.Buffer
+
+	tmpl, err := template.New("CSIDriver").Parse(csiDriver)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse template: %w", err)
+	}
+	err = tmpl.Execute(&buf, values)
+	if err != nil {
+		return "", fmt.Errorf("failed to replace values in template: %w", err)
+	}
+
+	return buf.String(), nil
+}
