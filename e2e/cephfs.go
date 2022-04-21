4adcc30f272b52cf2b41b5a9f3039decc2997641
Madhu Rajanna <madhupr007@gmail.com>
2022-04-21 10:57:48 +0530

    rebase: update gingko to v2 version
    Ginkgo v1 is deprecated and was replaced
    with the v2.
    
    Ref: https://onsi.github.io/ginkgo/
    MIGRATING_TO_V2#upgrading-to-ginkgo-20
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- e2e/cephfs.go
+++ e2e/cephfs.go
@@ -23,7 +23,7 @@ import (
 	"sync"
 
 	snapapi "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
-	. "github.com/onsi/ginkgo" // nolint
+	. "github.com/onsi/ginkgo/v2" // nolint
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
