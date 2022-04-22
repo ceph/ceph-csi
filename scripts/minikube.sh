41728c2465c43105ee0934a8cf1fd7fa0ed4d13c
Madhu Rajanna <madhupr007@gmail.com>
2022-04-22 19:15:57 +0530

    revert: "deploy: change image versions to v3.6.1"
    This reverts commit 2032a84c686a0dbaabddb798690aa28083682d0e.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -177,7 +177,7 @@ else
 fi
 
 # configure csi image version
-CSI_IMAGE_VERSION=${CSI_IMAGE_VERSION:-"v3.6.1"}
+CSI_IMAGE_VERSION=${CSI_IMAGE_VERSION:-"v3.6-canary"}
 
 #feature-gates for kube
 K8S_FEATURE_GATES=${K8S_FEATURE_GATES:-""}
