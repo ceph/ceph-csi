2cb52d6535fb289f1a73dea2744af85a014c2ee2
Humble Chirammal <hchiramm@redhat.com>
2022-05-26 11:26:35 +0530

    e2e: remove the generic ephemeral volume support check
    at present we have the check for kube version 1.21 in the tests
    which no longer required as it falls under supported kubernetes
    versions with the driver.
    
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

--- e2e/rbd.go
+++ e2e/rbd.go
@@ -715,33 +715,30 @@ var _ = Describe("RBD", func() {
 			})
 
 			By("verify generic ephemeral volume support", func() {
-				// generic ephemeral volume support is supported from 1.21
-				if k8sVersionGreaterEquals(f.ClientSet, 1, 21) {
-					// create application
-					app, err := loadApp(appEphemeralPath)
-					if err != nil {
-						e2elog.Failf("failed to load application: %v", err)
-					}
-					app.Namespace = f.UniqueName
-					err = createApp(f.ClientSet, app, deployTimeout)
-					if err != nil {
-						e2elog.Failf("failed to create application: %v", err)
-					}
-					// validate created backend rbd images
-					validateRBDImageCount(f, 1, defaultRBDPool)
-					validateOmapCount(f, 1, rbdType, defaultRBDPool, volumesType)
-					err = deletePod(app.Name, app.Namespace, f.ClientSet, deployTimeout)
-					if err != nil {
-						e2elog.Failf("failed to delete application: %v", err)
-					}
-					// validate created backend rbd images
-					validateRBDImageCount(f, 0, defaultRBDPool)
-					validateOmapCount(f, 0, rbdType, defaultRBDPool, volumesType)
-					// validate images in trash
-					err = waitToRemoveImagesFromTrash(f, defaultRBDPool, deployTimeout)
-					if err != nil {
-						e2elog.Failf("failed to validate rbd images in pool %s trash: %v", defaultRBDPool, err)
-					}
+				// create application
+				app, err := loadApp(appEphemeralPath)
+				if err != nil {
+					e2elog.Failf("failed to load application: %v", err)
+				}
+				app.Namespace = f.UniqueName
+				err = createApp(f.ClientSet, app, deployTimeout)
+				if err != nil {
+					e2elog.Failf("failed to create application: %v", err)
+				}
+				// validate created backend rbd images
+				validateRBDImageCount(f, 1, defaultRBDPool)
+				validateOmapCount(f, 1, rbdType, defaultRBDPool, volumesType)
+				err = deletePod(app.Name, app.Namespace, f.ClientSet, deployTimeout)
+				if err != nil {
+					e2elog.Failf("failed to delete application: %v", err)
+				}
+				// validate created backend rbd images
+				validateRBDImageCount(f, 0, defaultRBDPool)
+				validateOmapCount(f, 0, rbdType, defaultRBDPool, volumesType)
+				// validate images in trash
+				err = waitToRemoveImagesFromTrash(f, defaultRBDPool, deployTimeout)
+				if err != nil {
+					e2elog.Failf("failed to validate rbd images in pool %s trash: %v", defaultRBDPool, err)
 				}
 			})
 
