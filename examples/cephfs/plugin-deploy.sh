3f1395671120746ece5a7955b591831130ea045b
Michel van de Wouw <michelvandewouw@techforce1.nl>
2022-11-08 14:33:11 +0100

    plugin script csidriver added

--- examples/cephfs/plugin-deploy.sh
+++ examples/cephfs/plugin-deploy.sh
@@ -8,7 +8,7 @@ fi
 
 cd "$deployment_base" || exit 1
 
-objects=(csi-provisioner-rbac csi-nodeplugin-rbac csi-config-map csi-cephfsplugin-provisioner csi-cephfsplugin)
+objects=(csi-provisioner-rbac csi-nodeplugin-rbac csi-config-map csi-cephfsplugin-provisioner csi-cephfsplugin csidriver)
 
 for obj in "${objects[@]}"; do
 	kubectl create -f "./$obj.yaml"
