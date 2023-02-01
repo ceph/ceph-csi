60eb3c28c106d07f565d67018cd9d36709ef0871
Madhu Rajanna <madhupr007@gmail.com>
2023-02-01 18:10:01 +0100

    cleanup: fix static checks
    fix SA1019 static check to replace
    io/utils with os package and sets
    with generic sets
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- internal/util/idlocker.go
+++ internal/util/idlocker.go
@@ -33,14 +33,14 @@ const (
 // VolumeLocks implements a map with atomic operations. It stores a set of all volume IDs
 // with an ongoing operation.
 type VolumeLocks struct {
-	locks sets.String
+	locks sets.Set[string]
 	mux   sync.Mutex
 }
 
 // NewVolumeLocks returns new VolumeLocks.
 func NewVolumeLocks() *VolumeLocks {
 	return &VolumeLocks{
-		locks: sets.NewString(),
+		locks: sets.New[string](),
 	}
 }
 
