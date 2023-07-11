98fdadfde77adc16b578e550d4327b6d43ce0a6c
Rakshith R <rar@redhat.com>
2023-07-11 12:36:08 +0530

    rbd: do not execute rbd sparsify when volume is in use
    This commit makes sure sparsify() is not run when rbd
    image is in use.
    Running rbd sparsify with workload doing io and too
    frequently is not desirable.
    When a image is in use fstrim is run and sparsify will
    be run only when image is not mapped.
    
    Signed-off-by: Rakshith R <rar@redhat.com>

--- internal/rbd/diskusage.go
+++ internal/rbd/diskusage.go
@@ -23,7 +23,18 @@ import (
 // Sparsify checks the size of the objects in the RBD image and calls
 // rbd_sparify() to free zero-filled blocks and reduce the storage consumption
 // of the image.
+// This function will return ErrImageInUse if the image is in use, since
+// sparsifying an image on which i/o is in progress is not optimal.
 func (ri *rbdImage) Sparsify() error {
+	inUse, err := ri.isInUse()
+	if err != nil {
+		return fmt.Errorf("failed to check if image is in use: %w", err)
+	}
+	if inUse {
+		// if the image is in use, we should not sparsify it, return ErrImageInUse.
+		return ErrImageInUse
+	}
+
 	image, err := ri.open()
 	if err != nil {
 		return err
