da6d1dfee9ef56eec21a8efb48934282ca39528a
Madhu Rajanna <madhupr007@gmail.com>
2023-11-02 12:12:41 +0100

    e2e: add multiple labels to node
    update the e2e code to add multiple
    labels to the node at a time.
    
    fixes: #4146
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- e2e/rbd.go
+++ e2e/rbd.go
@@ -275,21 +275,14 @@ var _ = Describe("RBD", func() {
 		}
 		c = f.ClientSet
 		if deployRBD {
-			err := createNodeLabel(f, nodeRegionLabel, regionValue)
+			err := addLabelsToNodes(f, map[string]string{
+				nodeRegionLabel:          regionValue,
+				nodeZoneLabel:            zoneValue,
+				crushLocationRegionLabel: crushLocationRegionValue,
+				crushLocationZoneLabel:   crushLocationZoneValue,
+			})
 			if err != nil {
-				framework.Failf("failed to create node label: %v", err)
-			}
-			err = createNodeLabel(f, nodeZoneLabel, zoneValue)
-			if err != nil {
-				framework.Failf("failed to create node label: %v", err)
-			}
-			err = createNodeLabel(f, crushLocationRegionLabel, crushLocationRegionValue)
-			if err != nil {
-				framework.Failf("failed to create node label: %v", err)
-			}
-			err = createNodeLabel(f, crushLocationZoneLabel, crushLocationZoneValue)
-			if err != nil {
-				framework.Failf("failed to create node label: %v", err)
+				framework.Failf("failed to add node labels: %v", err)
 			}
 			if cephCSINamespace != defaultNs {
 				err = createNamespace(c, cephCSINamespace)
