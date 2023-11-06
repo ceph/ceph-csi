20a4a164ee1c12b4a4010365352da6fb3814f379
Riya Singhal <rsinghal@redhat.com>
2023-11-06 11:57:03 +0530

    cephfs: add test case for ipv6 address
    Signed-off-by: Riya Singhal <rsinghal@redhat.com>

--- internal/csi-addons/networkfence/fencing_test.go
+++ internal/csi-addons/networkfence/fencing_test.go
@@ -69,6 +69,11 @@ func TestFetchIP(t *testing.T) {
 			expectedErr: false,
 		},
 		{
+			clientInfo:  "client.4305 2001:0db8:85a3:0000:0000:8a2e:0370:7334:0/422650892",
+			expectedIP:  "2001:db8:85a3::8a2e:370:7334",
+			expectedErr: false,
+		},
+		{
 			clientInfo:  "",
 			expectedIP:  "",
 			expectedErr: true,
