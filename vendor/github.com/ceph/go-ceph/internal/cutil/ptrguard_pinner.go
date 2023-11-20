4ccb299fd5c3bfc7dcd42d0dc9e7ad6f1c6dc966
Riya Singhal <rsinghal@redhat.com>
2023-11-20 16:30:31 +0530

    rebase: update github.com/ceph/go-ceph to master
    this commit updates the version of go-ceph imported to
    latest, so we can make use of github.com/ceph/go-ceph/cephfs
    
    Signed-off-by: Riya Singhal <rsinghal@redhat.com>

--- vendor/github.com/ceph/go-ceph/internal/cutil/ptrguard_pinner.go
+++ vendor/github.com/ceph/go-ceph/internal/cutil/ptrguard_pinner.go
@@ -1,28 +1,26 @@
-//go:build !go1.21
-// +build !go1.21
-
-// This code assumes a non-moving garbage collector, which is the case until at
-// least go 1.20
+//go:build go1.21
+// +build go1.21
 
 package cutil
 
 import (
+	"runtime"
 	"unsafe"
 )
 
 // PtrGuard respresents a guarded Go pointer (pointing to memory allocated by Go
 // runtime) stored in C memory (allocated by C)
 type PtrGuard struct {
-	cPtr  CPtr
-	goPtr unsafe.Pointer
+	cPtr   CPtr
+	pinner runtime.Pinner
 }
 
 // NewPtrGuard writes the goPtr (pointing to Go memory) into C memory at the
 // position cPtr, and returns a PtrGuard object.
 func NewPtrGuard(cPtr CPtr, goPtr unsafe.Pointer) *PtrGuard {
 	var v PtrGuard
+	v.pinner.Pin(goPtr)
 	v.cPtr = cPtr
-	v.goPtr = goPtr
 	p := (*unsafe.Pointer)(unsafe.Pointer(cPtr))
 	*p = goPtr
 	return &v
@@ -33,5 +31,5 @@ func NewPtrGuard(cPtr CPtr, goPtr unsafe.Pointer) *PtrGuard {
 func (v *PtrGuard) Release() {
 	p := (*unsafe.Pointer)(unsafe.Pointer(v.cPtr))
 	*p = nil
-	v.goPtr = nil
+	v.pinner.Unpin()
 }
