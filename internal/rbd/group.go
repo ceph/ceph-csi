f9ab14e82685c2d3ba4709c17823c653a58e1584
Niels de Vos <ndevos@ibm.com>
2024-07-22 18:18:26 +0200

    rbd: check if an image is part of a group before adding it
    A RBD image can only be part of a single group. While an image is added
    to a group, check if the image is already part of a group, and return an
    error in case it is.
    
    Signed-off-by: Niels de Vos <ndevos@ibm.com>

--- internal/rbd/group.go
+++ internal/rbd/group.go
@@ -38,7 +38,28 @@ func (rv *rbdVolume) AddToGroup(ctx context.Context, vg types.VolumeGroup) error
 		return fmt.Errorf("could not get name for volume group %q: %w", vg, err)
 	}
 
-	return librbd.GroupImageAdd(ioctx, name, rv.ioctx, rv.RbdImageName)
+	// check if the image is already part of a group
+	// "rbd: ret=-17, File exists" is returned if the image is part of ANY group
+	image, err := rv.open()
+	if err != nil {
+		return fmt.Errorf("failed to open image %q: %w", rv, err)
+	}
+
+	info, err := image.GetGroup()
+	if err != nil {
+		return fmt.Errorf("could not get group information for image %q: %w", rv, err)
+	}
+
+	if info.Name != "" && info.Name != name {
+		return fmt.Errorf("image %q is already part of volume group %q", rv, info.Name)
+	}
+
+	err = librbd.GroupImageAdd(ioctx, name, rv.ioctx, rv.RbdImageName)
+	if err != nil {
+		return fmt.Errorf("failed to add image %q to volume group %q: %w", rv, vg, err)
+	}
+
+	return nil
 }
 
 // RemoveFromGroup removes the image from the group. This is called from the
