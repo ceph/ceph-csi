80371a6123e5824dc9d4fcc924f9995bab0e9943
yati1998 <ypadia@redhat.com>
2025-05-26 13:15:43 +0530

    rbd: enable setmetadata by default
    this enables setmetadata flag by default.
    
    fixes: #5328
    
    Signed-off-by: yati1998 <ypadia@redhat.com>

--- PendingReleaseNotes.md
+++ PendingReleaseNotes.md
@@ -1,10 +1,9 @@
 # v3.15 Pending Release Notes
 
-## Breaking Changes
-
-- Support for `secret.adminID` and `secret.adminKey` has been deprecated and removed
-  from Helm charts. Please use `secret.userID` and `secret.userKey` instead.
+## Breaking changes
 
 ## Features
 
 ## NOTE
+
+- `--setmetadata` flag has been set to true by default.
--- cmd/cephcsi.go
+++ cmd/cephcsi.go
@@ -74,7 +74,7 @@ func init() {
 	flag.StringVar(&conf.PluginPath, "pluginpath", defaultPluginPath, "plugin path")
 	flag.StringVar(&conf.StagingPath, "stagingpath", defaultStagingPath, "staging path")
 	flag.StringVar(&conf.ClusterName, "clustername", "", "name of the cluster")
-	flag.BoolVar(&conf.SetMetadata, "setmetadata", false, "set metadata on the volume")
+	flag.BoolVar(&conf.SetMetadata, "setmetadata", true, "set metadata on the volume")
 	flag.StringVar(&conf.InstanceID, "instanceid", "default", "Unique ID distinguishing this instance of Ceph-CSI"+
 		" among other instances, when sharing Ceph clusters across CSI instances for provisioning")
 	flag.IntVar(&conf.PidLimit, "pidlimit", 0, "the PID limit to configure through cgroups")
