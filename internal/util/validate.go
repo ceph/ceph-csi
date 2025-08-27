789444d3de5a104eb9af446a2a2963f47cb78d7a
gadi-didi <gadi.didi@ibm.com>
2025-08-27 13:10:54 +0300

    util: added validate methods
    added basic ValidateControllerPublishVolumeRequest and ValidateControllerUnpublishVolumeRequest
    
    Signed-off-by: gadi-didi <gadi.didi@ibm.com>

--- internal/util/validate.go
+++ internal/util/validate.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The Ceph-CSI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package util
 
 import (
@@ -5,6 +21,35 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+// ValidateControllerPublishVolumeRequest validates the controller publish request.
+func ValidateControllerPublishVolumeRequest(req *csi.ControllerPublishVolumeRequest) error {
+	if req.GetVolumeId() == "" {
+		return status.Error(codes.InvalidArgument, "volume ID missing in request")
+	}
+
+	if req.GetVolumeCapability() == nil {
+		return status.Error(codes.InvalidArgument, "volume capability missing in request")
+	}
+	if req.GetNodeId() == "" {
+		return status.Error(codes.InvalidArgument, "node ID missing in request")
+	}
+
+	return nil
+}
+
+// ValidateControllerUnpublishVolumeRequest validates the controller unpublish request.
+func ValidateControllerUnpublishVolumeRequest(req *csi.ControllerUnpublishVolumeRequest) error {
+	if req.GetVolumeId() == "" {
+		return status.Error(codes.InvalidArgument, "volume ID missing in request")
+	}
+
+	if req.GetNodeId() == "" {
+		return status.Error(codes.InvalidArgument, "node ID missing in request")
+	}
+
+	return nil
+}
 
 // ValidateNodeStageVolumeRequest validates the node stage request.
 func ValidateNodeStageVolumeRequest(req *csi.NodeStageVolumeRequest) error {
