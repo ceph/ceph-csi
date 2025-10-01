fb3ef82498d7fd8f6468f57120ddccd4f1338b5b
Niels de Vos <ndevos@ibm.com>
2025-10-01 17:02:23 +0200

    e2e: add NVMe-oF gateway deployment
    Rook is not yet able to deploy the NVMe-oF gateway, so it needs to be
    done manually.
    
    When the gateway starts, it uses a pool for fetching/storing some
    metadata. If the pool does not exist, the gateway fails to start.
    Before the gateway is deployed the `nvmeofpool` is created (and deleted
    after testing).
    
    Signed-off-by: Niels de Vos <ndevos@ibm.com>

---
+++ e2e/nvmeof.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2025 The Ceph-CSI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	"k8s.io/kubernetes/test/e2e/framework"
+	"k8s.io/pod-security-admission/api"
+)
+
+const (
+	nvmeofPool = "nvmeofpool"
+)
+
+var _ = Describe("nvmeof", func() {
+	f := framework.NewDefaultFramework("nvmeof")
+	f.NamespacePodSecurityEnforceLevel = api.LevelPrivileged
+
+	BeforeEach(func() {
+		if !testNVMeoF {
+			Skip("Skipping NVMe-oF E2E")
+		}
+
+		if deployNVMeoF {
+			deployGateway(f, deployTimeout)
+		}
+	})
+
+	AfterEach(func() {
+		if !testNVMeoF {
+			Skip("Skipping NVMe E2E")
+		}
+
+		if deployNVMeoF {
+			deleteGateway(f)
+		}
+	})
+
+	Context("Test NVMe CSI", func() {
+		if !testNVMeoF {
+			return
+		}
+
+		It("Test NVMe-oF CSI", func() {
+			Skip("no NVMe-oF test cases yet")
+		})
+	})
+})
