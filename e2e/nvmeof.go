b5604c5cad4f9748b0be2c9471282d64448d80d5
Niels de Vos <ndevos@ibm.com>
2025-10-03 16:55:03 +0200

    e2e: add NVMe-oF volume creation test
    Create a PVC and wait for it to get Bound. This does not exersice the
    node-plugin yet, only the controller/provisioner.
    
    Signed-off-by: Niels de Vos <ndevos@ibm.com>
    (cherry picked from commit dbe85c7a1a64e00ed60220cbf5b40f0afc5e8053)

--- e2e/nvmeof.go
+++ e2e/nvmeof.go
@@ -17,7 +17,8 @@ limitations under the License.
 package e2e
 
 import (
-	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/kubernetes/test/e2e/framework"
 	"k8s.io/pod-security-admission/api"
@@ -27,48 +28,88 @@ const (
 	nvmeofPool = "nvmeofpool"
 )
 
-var _ = Describe("nvmeof", func() {
+var _ = ginkgo.Describe("nvmeof", func() {
+	if !testNVMeoF {
+		framework.Logf("Skipping NVMe-oF E2E")
+
+		return
+	}
+
 	f := framework.NewDefaultFramework("nvmeof")
 	f.NamespacePodSecurityEnforceLevel = api.LevelPrivileged
 
-	BeforeEach(func() {
-		if !testNVMeoF {
-			Skip("Skipping NVMe-oF E2E")
+	// is set during BeforeEach(), f.UniqueName is empty here
+	var nvmeofStorageClass string
+
+	ginkgo.BeforeEach(func() {
+		if !deployNVMeoF {
+			return
+		}
+
+		// FIXME: gateway should get deployed by Rook
+		deployGateway(f, deployTimeout)
+
+		// No need to create the namespace if ceph-csi is deployed via helm or operator.
+		if cephCSINamespace != defaultNs && !(helmTest || operatorDeployment) {
+			err := createNamespace(f.ClientSet, cephCSINamespace)
+			if err != nil {
+				logAndFail("failed to create namespace: %v", err)
+			}
 		}
 
 		// Ceph credentials referenced in the StorageClass
 		createNVMeoFCredentials(f)
 
-		if deployNVMeoF {
-			deployGateway(f, deployTimeout)
-			deployNVMeoFPlugin(f, deployTimeout)
+		// FIXME: use ceph-csi-operator
+		deployNVMeoFPlugin(f, deployTimeout)
+
+		// create the StorageClass
+		options := map[string]string{}
+		params := map[string]string{
+			"pool": nvmeofPool,
 		}
+		policy := v1.PersistentVolumeReclaimDelete
+
+		nvmeofStorageClass = "e2e-" + f.UniqueName + "-sc"
+		createNVMeoFStorageClass(f, nvmeofStorageClass, options, params, policy)
 	})
 
-	AfterEach(func() {
-		if !testNVMeoF {
-			Skip("Skipping NVMe E2E")
-		}
-
-		if deployNVMeoF {
-			deleteNVMeoFPlugin()
-			deleteGateway(f)
-		}
-	})
-
-	Context("Test NVMe CSI", func() {
-		if !testNVMeoF {
+	ginkgo.AfterEach(func() {
+		if !deployNVMeoF {
 			return
 		}
 
-		It("Test NVMe-oF CSI", func() {
-			options := map[string]string{}
-			params := map[string]string{}
-			policy := v1.PersistentVolumeReclaimDelete
+		deleteNVMeoFPlugin()
+		deleteGateway(f)
+		deleteNVMeofStorageClass(f, nvmeofStorageClass)
+	})
 
-			createNVMeoFStorageClass(f, "e2e-nvmeof", options, params, policy)
+	ginkgo.Context("Test NVMe CSI", func() {
 
-			Skip("no NVMe-oF test cases yet")
+		pvcPath := nvmeofExamplePath + "pvc.yaml"
+
+		ginkgo.It("create a PVC and delete it", func() {
+			ginkgo.By("prepare PVC")
+			pvc, err := loadPVC(pvcPath)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			pvc.Namespace = f.UniqueName
+			pvc.Spec.StorageClassName = &nvmeofStorageClass
+
+			ginkgo.By("create the PVC")
+			err = createPVCAndvalidatePV(f.ClientSet, pvc, deployTimeout)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			validateRBDImageCount(f, 1, nvmeofPool)
+			validateOmapCount(f, 1, rbdType, nvmeofPool, volumesType)
+
+			ginkgo.By("delete the PVC again")
+			err = deletePVCAndValidatePV(f.ClientSet, pvc, deployTimeout)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			// validate created backend rbd images
+			validateRBDImageCount(f, 0, nvmeofPool)
+			validateOmapCount(f, 0, rbdType, nvmeofPool, volumesType)
 		})
 	})
 })
