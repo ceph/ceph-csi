6ec3241bcd5146260917d0e82366acda76cafb12
Praveen M <m.praveen@ibm.com>
2026-03-23 11:50:05 +0530

    rbd: use StorageClass image features for temp clones and snapshots
    Previously, temporary clone images (-temp) and snapshot backing images
    had their features hard-coded to only layering and deep-flatten.
    
    Inherit the image features from the parent volume (configured via
    StorageClass imageFeatures) while ensuring layering and deep-flatten
    are always enabled, as they are required for the flatten operation.
    
    Signed-off-by: Praveen M <m.praveen@ibm.com>
    (cherry picked from commit 28daf2e0d1d4ab4e15d4b3c30ee3df6522111de5)

--- internal/rbd/controllerserver.go
+++ internal/rbd/controllerserver.go
@@ -1391,9 +1391,10 @@ func (cs *ControllerServer) doSnapshotClone(
 	// generate cloned volume details from snapshot
 	cloneRbd := rbdSnap.toVolume()
 	defer cloneRbd.Destroy(ctx)
-	// add image feature for cloneRbd
+	// Use the parent volume's image features and ensure that layering and
+	// deep-flatten are always enabled for the snapshot backing image.
 	f := []string{librbd.FeatureNameLayering, librbd.FeatureNameDeepFlatten}
-	cloneRbd.ImageFeatureSet = librbd.FeatureSetFromNames(f)
+	cloneRbd.ImageFeatureSet = parentVol.ImageFeatureSet | librbd.FeatureSetFromNames(f)
 
 	err := cloneRbd.Connect(cr)
 	if err != nil {
