7016901bc4c6fba619b88af66736a6b4585cefa2
Praveen M <m.praveen@ibm.com>
2026-07-07 15:12:18 +0530

    e2e: migrate VolumeGroupSnapshot API from v1beta2 to v1
    The VolumeGroupSnapshot API has been promoted to v1 in
    external-snapshotter v8. Migrate the e2e tests to use the
    stable v1 API and client instead of v1beta2.
    
    Signed-off-by: Praveen M <m.praveen@ibm.com>

--- e2e/volumegroupsnapshot.go
+++ e2e/volumegroupsnapshot.go
@@ -21,7 +21,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 
-	groupsnapapi "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumegroupsnapshot/v1beta2"
+	groupsnapapi "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumegroupsnapshot/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/kubernetes/test/e2e/framework"
 )
