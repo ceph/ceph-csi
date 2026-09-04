73a1e16ae5500a3af60c436ac778830ef724006b
Niels de Vos <ndevos@ibm.com>
2026-09-04 09:44:50 +0200

    ci: add AGENTS.md with guidance for AI agents
    Document the purpose of the ci/centos branch, the CI workflow, all
    Jenkins jobs, linting/testing commands, how to add or modify jobs, and
    the commit message conventions for this branch.
    
    Assisted-By: AskBob <askbob@ibm.com>
    Signed-off-by: Niels de Vos <ndevos@ibm.com>

--- scripts/mdl-style.rb
+++ scripts/mdl-style.rb
@@ -3,7 +3,7 @@ all
 #Refer below url for more information about the markdown rules.
 #https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
 
-rule 'MD013', :code_blocks => false, :tables => false
+rule 'MD013', :ignore_code_blocks => true, :tables => false, :line_length => 120
 
 exclude_rule 'MD040' # Fenced code blocks should have a language specified
 exclude_rule 'MD041' # First line in file should be a top level header
